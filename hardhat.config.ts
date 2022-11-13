8738a4f1c3089103d89b05e0f98f6c3f30ef199e
yano <developer613@sourceplay.com>
2022-11-14 07:34:03 +0900

    update package.json and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.2",
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
